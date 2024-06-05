825f27945ba40e82c13a01fdf3a299036d20d5fb
Margo Urey <margo@margonline.co.uk>
2024-06-05 13:52:39 +0100

    Continue project set up (#1)
    * Remove default setup by vite/vuejs
    
    * Initial routing and views for home and projects pages
    
    * Lazy load projects route using dynamic imports
    
    * Create a dynamic route for single project pages
    
    * Initial installation of supabase. Create a supabase client using the supabase-js lib.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
+<script setup lang="ts"></script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Home page</h1>
+    <RouterLink to="/projects">Projects page</RouterLink>
+  </div>
 </template>
