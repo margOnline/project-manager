2d9cae7e444b66bccbd3cf5162ab6c21bb9e5e92
Margo Urey <margo@margonline.co.uk>
2024-06-06 17:22:39 +0100

    Refactor to use file based routing with 'uplugin-vue-router'

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,6 @@
 <template>
   <div>
     <h1>Home page</h1>
-    <RouterLink to="/projects">Projects page</RouterLink>
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
   </div>
 </template>
