d467d547e32755c1536b07311ad98d9a3fb70f36
Margo Urey <margo@margonline.co.uk>
2024-07-10 14:36:31 +0100

    Populate projects page with data from the database (#5)
    * Amend seed file to be more helpful and configurable
    * Use an IIFE to fetch the project data
    * Generate types for the supabase d/b and the projects table

--- database/seed.js
+++ database/seed.js
@@ -5,19 +5,41 @@ import { config } from '@dotenvx/dotenvx'
 config()
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
+
+const logErrorAndExit = (tableName, error) => {
+  console.error(
+    `An error occurred in table '${tableName} with code ${error.code}: ${error.message}`
+  )
+  process.exit(1)
+}
+
+const logStep = (stepMessage) => {
+  console.log(stepMessage)
+}
+
 const seedProjects = async (numOfEntries) => {
-  const arrayOfInserts = []
+  logStep('Seeding projects...')
+  const projects = []
+
   for (let i = 0; i < numOfEntries; i++) {
     const name = faker.lorem.words(3)
-    const projectObject = {
+    projects.push({
       name,
       slug: name.toLocaleLowerCase().replace(/ /g, '-'),
       status: faker.helpers.arrayElement(['in-progress', 'completed']),
       collaborators: faker.helpers.arrayElements([1, 2, 3])
-    }
-    arrayOfInserts.push(projectObject)
+    })
   }
-  await supabase.from('projects').insert(arrayOfInserts)
+  const { data, error } = await supabase.from('projects').insert(projects).select('id')
+  if (error) return logErrorAndExit('Projects', error)
+  logStep('Projects seeded successfully.')
+  return data
 }
 
-await seedProjects(10)
+const seedDatabase = async (numOfEntriesPerTable) => {
+  await seedProjects(numOfEntriesPerTable)
+}
+
+const numOfEntriesPerTable = 11
+
+seedDatabase(numOfEntriesPerTable)
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { supabase } from '@/lib/supabaseClient'
+import type { Tables } from '../../../database/types'
+import { RouterLink } from 'vue-router'
+
+const projects = ref<Tables<'projects'>[] | null>(null)
+
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) {
+    console.error(error)
+  }
+
+  projects.value = data
+})()
+</script>
 <template>
   <div>
     <h1>Projects page</h1>
-    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/">Go to home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+    </ul>
   </div>
 </template>
