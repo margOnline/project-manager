6201b7e2b3a8cacd942209d29149515879206797
Margo Urey <margo@margonline.co.uk>
2024-07-10 15:31:42 +0100

    Update seed file to create some tasks

--- database/seed.js
+++ database/seed.js
@@ -36,8 +36,33 @@ const seedProjects = async (numOfEntries) => {
   return data
 }
 
+const seedTasks = async (numEntries, projectsIds) => {
+  logStep('Seeding tasks...')
+  const tasks = []
+
+  for (let i = 0; i < numEntries; i++) {
+    tasks.push({
+      name: faker.lorem.words(3),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      description: faker.lorem.paragraph(),
+      due_date: faker.date.future(),
+      project_id: faker.helpers.arrayElement(projectsIds),
+      collaborators: faker.helpers.arrayElements([1, 2, 3])
+    })
+  }
+
+  const { data, error } = await supabase.from('tasks').insert(tasks).select('id')
+
+  if (error) return logErrorAndExit('Tasks', error)
+
+  logStep('Tasks seeded successfully')
+
+  return data
+}
+
 const seedDatabase = async (numOfEntriesPerTable) => {
-  await seedProjects(numOfEntriesPerTable)
+  const projectsIds = (await seedProjects(numOfEntriesPerTable)).map((project) => project.id)
+  await seedTasks(numOfEntriesPerTable, projectsIds)
 }
 
 const numOfEntriesPerTable = 11
