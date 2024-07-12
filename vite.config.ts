2571121dab0dff6e7e4eeb4b95aae1924127323d
Margo Urey <margo@margonline.co.uk>
2024-07-12 16:49:48 +0100

    Configurations for shadcn and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -7,6 +9,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [VueRouter(), vue()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
