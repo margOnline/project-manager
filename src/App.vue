83de48ff6f3fcf14fe8a7f94ba24a2bf38a30a66
Margo Urey <margo@margonline.co.uk>
2024-07-12 17:04:01 +0100

    Add the nav element with a search bar and avatar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/60 flex justify-between px-6 items-center">
+    <form class="w-full max-w-96">
+      <input type="text" class="w-full bg-background pl-8" placeholder="Search ..." />
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
