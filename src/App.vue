8bb61f05b1959a4bab36b4507cd70beb233e40d8
Margo Urey <margo@margonline.co.uk>
2024-07-15 19:42:10 +0100

    Position the search icon to be vertically aligned with the search bar (#8)

--- src/App.vue
+++ src/App.vue
@@ -13,26 +13,38 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 <template>
   <nav class="h-16 border-b bg-muted/60 flex justify-between px-6 items-center">
-    <form class="w-full max-w-96">
-      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-        <g
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-        >
-          <circle cx="11" cy="11" r="8" />
-          <path d="m21 21l-4.3-4.3" />
-        </g>
-      </svg>
+    <form class="relative h-fit w-full max-w-96">
+      <span class="absolute top-3 left-2.5 text-muted-foreground">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <circle cx="11" cy="11" r="8" />
+            <path d="m21 21l-4.3-4.3" />
+          </g>
+        </svg>
+      </span>
       <Input type="text" class="w-full bg-background pl-8" placeholder="Search ..." />
     </form>
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar>
-          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-          <AvatarFallback>CN</AvatarFallback>
+          <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+            >
+              <circle cx="12" cy="8" r="5" />
+              <path d="M20 21a8 8 0 0 0-16 0" />
+            </g>
+          </svg>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
