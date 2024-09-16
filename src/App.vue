86cdeb586be78239b04819ad80ef0fab313fd3b4
Margo Urey <margo@margonline.co.uk>
2024-09-16 15:22:42 +0100

    Use suspense (#14)
    * Make use of the suspense component to enable async components
    to load with a fallback component and for more elegant
    code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
-<script setup lang="ts">
-import TopNavbar from '@/components/Layout/TopNavbar.vue'
-import Sidebar from '@/components/Layout/Sidebar.vue'
-</script>
-
+<script setup lang="ts"></script>
 <template>
-  <Sidebar />
-  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavbar />
-
-    <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
-      <div class="flex items-center">
-        <h1 class="text-lg font-semibold md:text-2xl">Page Title</h1>
-      </div>
-      <RouterView />
-    </main>
-  </div>
+  <AuthLayout>
+    <RouterView v-slot="{ Component, route }">
+      <Suspense v-if="Component" timeout="0">
+        <Component :is="Component" :key="route.name" />
+        <template #fallback>
+          <span>Loading ...</span>
+        </template>
+      </Suspense>
+    </RouterView>
+  </AuthLayout>
 </template>
