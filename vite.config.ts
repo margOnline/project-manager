a060f33aeb98871a3563a2ff5bc2c0f15cea6038
margOnline <margourey@gmail.com>
2024-09-16 13:07:11 +0100

    Remove the need to repetitively import components: - Integrate unplugin-vue-components which automatically imports components by referencing components in the src/components directiory. When running npm dev the plugin scans this directory and generates a components.d.ts file which will have the declarations for the types of components it found.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -30,6 +31,9 @@ export default defineConfig({
           isCustomElement: (element) => element.startsWith('iconify-icon')
         }
       }
+    }),
+    Components({
+      /* options */
     })
   ],
   css: {
