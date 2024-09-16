433da8b59bd92842fd9c18e2c2622c7f2cb59664
margOnline <margourey@gmail.com>
2024-09-16 12:39:30 +0100

    Configure unplugin-auto-import to work with unplugin-vue-router
    Update unplugin-vue-router to the latest version

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -11,6 +11,7 @@ declare global {
   const customRef: typeof import('vue')['customRef']
   const defineAsyncComponent: typeof import('vue')['defineAsyncComponent']
   const defineComponent: typeof import('vue')['defineComponent']
+  const definePage: typeof import('unplugin-vue-router/runtime')['definePage']
   const effectScope: typeof import('vue')['effectScope']
   const getCurrentInstance: typeof import('vue')['getCurrentInstance']
   const getCurrentScope: typeof import('vue')['getCurrentScope']
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +20,7 @@ export default defineConfig({
         /\.vue\?vue/, // .vue
         /\.md$/ // .md
       ],
-      imports: ['vue', 'vue-router'],
+      imports: ['vue', VueRouterAutoImports],
       dts: true,
       viteOptimizeDeps: true
     }),
