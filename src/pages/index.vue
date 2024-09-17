d1e99e7262b222599ff89b66f565d2d5adcd0d00
Margo Urey <margo@margonline.co.uk>
2024-09-17 16:19:06 +0100

    Dynamic page titles (#15)
    Using pinia for state management, create a pageStore
    to store a title property which each page can set and access
    
    Configure vite to auto import stores and pinia
    so we dont have to manually import them in every page that uses them.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+usePageStore().pageData.title = 'Home page'
+</script>
 <template>
   <div>
-    <h1>Home page</h1>
+    <h1>Some text</h1>
   </div>
 </template>
--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -4,6 +4,8 @@ import type { Tables } from '../../../database/types'
 import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
 
+usePageStore().pageData.title = 'My tasks'
+
 const tasks = ref<Tables<'tasks'>[] | null>(null)
 
 const getTasks = async () => {
