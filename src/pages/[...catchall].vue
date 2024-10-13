0a8fb189cd521ebc3375b480d9a82ae38c9ccdc5
margOnline <margourey@gmail.com>
2024-10-13 11:40:09 +0100

    Create component to be used for displaying errors

--- src/pages/[...catchall].vue
+++ src/pages/[...catchall].vue
@@ -1,12 +1,8 @@
+<script setup lang="ts">
+import AppErrorPage from '@/components/AppError/AppErrorPage.vue'
+</script>
 <script setup lang="ts"></script>
-<template>
-  <div>
-    <h2>404 Not Found</h2>
-  </div>
-</template>
 
-<style scoped>
-h2 {
-  color: red;
-}
-</style>
+<template>
+  <AppErrorPage />
+</template>
