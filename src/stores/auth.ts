d1aef4e5c639898b4e902105b16b648cb13b89bf
margOnline <margourey@gmail.com>
2024-10-14 12:47:15 +0100

    Store the user and profile in a global store for access throughout the app

---
+++ src/stores/auth.ts
@@ -0,0 +1,12 @@
+import type { User } from '@supabase/supabase-js'
+import type { Tables } from 'database/types'
+
+export const useAuthStore = defineStore('auth-store', () => {
+  const user = ref<null | User>(null)
+  const profile = ref<null | Tables<'profiles'>>(null)
+
+  return {
+    user,
+    profile
+  }
+})
