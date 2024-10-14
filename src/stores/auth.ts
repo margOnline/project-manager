9aad3c66e6f226b038bb3ce320bcac2b337e8f34
margOnline <margourey@gmail.com>
2024-10-14 13:50:15 +0100

    After login or registering, store the session data in the global auth store

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,12 +1,21 @@
-import type { User } from '@supabase/supabase-js'
+import type { Session, User } from '@supabase/supabase-js'
 import type { Tables } from 'database/types'
 
 export const useAuthStore = defineStore('auth-store', () => {
   const user = ref<null | User>(null)
   const profile = ref<null | Tables<'profiles'>>(null)
 
+  const setAuth = (userSession: null | Session = null) => {
+    if (!userSession) {
+      user.value = null
+      return
+    }
+    user.value = userSession.user
+  }
+
   return {
     user,
-    profile
+    profile,
+    setAuth
   }
 })
